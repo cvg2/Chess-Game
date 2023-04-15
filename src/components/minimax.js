af7f81dbbbdd48863b6fc31312d821874a8d501f
Ian Hennings <33659232+IanHennings88@users.noreply.github.com>
2023-04-15 12:44:40 -0400

    Update minimax.js
    There is more to implement. This is just the basics.

--- src/components/minimax.js
+++ src/components/minimax.js
@@ -3,3 +3,36 @@ function minimax(board, depth, maximizingPlayer) {
     // if we've reached the maximum search depth or the game is over, return the heuristic value of the current board state
     return evaluateBoard(board);
   }
+
+    if (maximizingPlayer) {
+    let bestValue = -Infinity;
+    for (let move of generatePossibleMoves(board)) {
+      let child = makeMove(board, move);
+      let value = minimax(child, depth - 1, false);
+      bestValue = Math.max(bestValue, value);
+    }
+    return bestValue;
+  } else {
+    let bestValue = Infinity;
+    for (let move of generatePossibleMoves(board)) {
+      let child = makeMove(board, move);
+      let value = minimax(child, depth - 1, true);
+      bestValue = Math.min(bestValue, value);
+    }
+    return bestValue;
+  }
+}
+
+function findBestMove(board, depth) {
+  let bestMove;
+  let bestValue = -Infinity;
+  for (let move of generatePossibleMoves(board)) {
+    let child = makeMove(board, move);
+    let value = minimax(child, depth - 1, false);
+    if (value > bestValue) {
+      bestValue = value;
+      bestMove = move;
+    }
+  }
+  return bestMove;
+}
