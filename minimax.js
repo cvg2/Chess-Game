14b96e9342d5b7ffb3aec4d2b13fd0446509999c
Ian Hennings <33659232+IanHennings88@users.noreply.github.com>
2023-04-23 22:36:42 -0400

    Update minimax.js
    This file is deprecated from the project. This is not being used.

--- minimax.js
+++ minimax.js
@@ -1,4 +1,6 @@
-function minimax(board, depth, maximizingPlayer) {
+//This .js file has been deprecated and is no longer considered in use
+
+/*function minimax(board, depth, maximizingPlayer) {
   if (depth === 0 || gameIsOver(board)) {
     // if we've reached the maximum search depth or the game is over, return the heuristic value of the current board state
     return evaluateBoard(board);
@@ -36,3 +38,4 @@ function findBestMove(board, depth) {
   }
   return bestMove;
 }
+*/
